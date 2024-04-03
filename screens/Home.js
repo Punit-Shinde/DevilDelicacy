8b6738c048e817bc13984f9591adbbd27f44f7bb
Punit Shinde <punitshinde1@gmail.com>
2024-04-03 16:55:29 +0530

    Layout done, assets saved in constants

---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { View, SafeAreaView, FlatList, Text } from "react-native";
+
+import { COLORS, NFTData } from "../constants";
+
+const Home = () => {
+  return <Text>Home</Text>;
+};
+
+export default Home;
